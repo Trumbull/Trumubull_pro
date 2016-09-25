b3cef4abd636c0d33d49d39b8c5dda18380365b9
Trumbull <slavka-dol@ya.ru>
2016-09-25 19:12:15 +0300

    вапвпвпвапв

---
+++ Airport/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Airport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.tableGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Заказ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Информация о заказах";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(30, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 38);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Информация о клиентах";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tableGrid
+            // 
+            this.tableGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableGrid.Location = new System.Drawing.Point(186, 40);
+            this.tableGrid.Name = "tableGrid";
+            this.tableGrid.Size = new System.Drawing.Size(732, 354);
+            this.tableGrid.TabIndex = 3;
+            this.tableGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 428);
+            this.Controls.Add(this.tableGrid);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tableGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView tableGrid;
+    }
+}
+
---
+++ Airport/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Collections;
+
+namespace Airport
+{
+    public partial class Form1 : Form
+    {
+        int g = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tableGrid.Columns.Add("0", "Id");
+            tableGrid.Columns.Add("1", "Фамилия");
+            tableGrid.Columns.Add("2", "Отчество");
+            tableGrid.Columns.Add("3", "Дата рождения");
+            tableGrid.Columns.Add("4", "Мили");
+            tableGrid.Columns.Add("5", "Страна");
+            tableGrid.Visible = false;
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Deletetable();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Deletetable();
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (g < 1)
+            {
+                g = 1;
+                tableGrid.Visible = true;
+                MySqlCommand command = new MySqlCommand();
+                string connectionString, commandString;
+                connectionString = "Data source=127.0.0.1; UserId=root; Password=;database=mydb;";
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                commandString = "SELECT * FROM mydb.strana;";
+                command.CommandText = commandString;
+                command.Connection = connection;
+                MySqlDataReader reader;
+                Hashtable codes = new Hashtable();
+
+
+                try
+                {
+                    command.Connection.Open();
+                    reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        codes.Add(reader["Strana_id"], reader["Nazvanie"]);
+                        //codes.Add(reader["Nazvanie"], reader["ISO_3166"]);
+
+
+                    }
+                    reader.Close();
+                }
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine("Error: \r\n{0}", ex.ToString());
+                }
+                finally
+                {
+                    command.Connection.Close();
+                }
+
+
+                commandString = "SELECT * FROM mydb.klient;";
+                command.CommandText = commandString;
+                command.Connection = connection;
+                try
+                {
+                    command.Connection.Open();
+                    reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int rowNumber = tableGrid.Rows.Add();
+                        tableGrid.Rows[rowNumber].Cells[0].Value = reader["Imja"];
+                        tableGrid.Rows[rowNumber].Cells[1].Value = reader["Familiya"];
+                        tableGrid.Rows[rowNumber].Cells[2].Value = reader["Otshestvo"];
+                        tableGrid.Rows[rowNumber].Cells[3].Value = reader["Data_rogdeniya"];
+                        tableGrid.Rows[rowNumber].Cells[4].Value = reader["Mili"];
+                        tableGrid.Rows[rowNumber].Cells[5].Value = codes[reader["Strana_id"]];
+                    }
+                    reader.Close();
+                }
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine("Error: \r\n{0}", ex.ToString());
+                }
+                finally
+                {
+                    command.Connection.Close();
+                }
+                codes.Clear();
+            }
+            
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+
+        }
+        private void Deletetable()
+        {
+
+            while (tableGrid.Rows.Count > 1)
+                for (int i = 0; i < tableGrid.Rows.Count - 1; i++)
+                    tableGrid.Rows.Remove(tableGrid.Rows[i]);
+            g = 0;
+            tableGrid.Visible = false;
+        }
+
+    }
+}
